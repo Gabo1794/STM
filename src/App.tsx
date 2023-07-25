57ddea6be939fb767343270236e1d797a0f62e88
Gabriel Montano <sonic1794@gmail.com>
2023-07-25 17:56:55 -0600

    adding packages to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button, ConfigProvider } from 'antd';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ConfigProvider
+    theme={{
+      token: {
+        colorPrimary: '#00b96b',
+      },
+    }}
+  >
+    <Router>
+      <Routes>
+        <Route path="/" Component={() => <div>hola mundo</div>}/>
+        <Route path="/dashboard" Component={() => <div>hola mundo2</div>}/>
+        {/* <Route path="/dashboard/:params" Component={() => <div>hola mundo2</div>}/> const { postSlug } = useParams(); */}
+        <Route path="*" Component={() => <div>Not found</div>}/>
+      </Routes>
+    </Router>
+  </ConfigProvider>
   )
 }
 
